b48e1311f5b09d870a70ad28fb2a4903b3be6936
Fabio <alessf13@gmail.com>
2021-09-05 17:54:02 +0200

    feat: findAll added to CommentRepository

--- src/main/java/tech/makers/twitter/comment/CommentRepository.java
+++ src/main/java/tech/makers/twitter/comment/CommentRepository.java
@@ -2,5 +2,8 @@ package tech.makers.twitter.comment;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends CrudRepository {
+    List<Comment> findAll();
 }
