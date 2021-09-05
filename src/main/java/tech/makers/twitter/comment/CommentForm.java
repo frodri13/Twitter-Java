4b361c1f5c670bef2c55968027db1b628c383d60
Fabio <alessf13@gmail.com>
2021-09-05 17:31:53 +0200

    feat: commentForm class created

--- src/main/java/tech/makers/twitter/comment/CommentForm.java
+++ src/main/java/tech/makers/twitter/comment/CommentForm.java
@@ -1,4 +1,19 @@
 package tech.makers.twitter.comment;
 
 public class CommentForm {
+    private String body;
+
+    protected CommentForm() {}
+
+    CommentForm(String body){
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
